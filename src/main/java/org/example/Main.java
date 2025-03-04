0f4b11d224934b2120cc96aed61877dc4c5b5408
mancelina24 <feyza.turkay@ogr.iu.edu.tr>
2025-03-04 15:35:32 +0300

    isPerfectNumber

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,14 +7,27 @@ public class Main {
     public static boolean isPalindrome(int number) {
         int originalNumber=Math.abs(number);
         int reverseNumber=0;
-        int temp=originalNumber;
 
-        while (temp>0){
+
+        while (originalNumber>0){
             int lastDigit=number %10;
             reverseNumber=reverseNumber * 10+lastDigit;
-            temp/=10;
+            originalNumber/=10;
         }
         return originalNumber==reverseNumber;
-
     }
+public static boolean isPerfectNumber(int number){
+        if (number<0){
+            return false;
+        }
+        int sum=0;
+
+        for (int i=1; i<=number/2; i++) {
+            if(number % i ==0){
+                sum+=i;
+            }
+        }
+        return sum==number;
+}
+
 }
