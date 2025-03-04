8c91912b698fd13bb9b2d7b4f809359a31c3c848
mancelina24 <feyza.turkay@ogr.iu.edu.tr>
2025-03-04 15:21:42 +0300

    isPalindrome

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static boolean isPalindrome(int number) {
+        int originalNumber=Math.abs(number);
+        int reverseNumber=0;
+        int temp=originalNumber;
+
+        while (temp>0){
+            int lastDigit=number %10;
+            reverseNumber=reverseNumber * 10+lastDigit;
+            temp/=10;
+        }
+        return originalNumber==reverseNumber;
+
+    }
 }
