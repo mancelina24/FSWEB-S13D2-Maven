c9940d93cdecdd350de026594502706d2409a6cb
mancelina24 <feyza.turkay@ogr.iu.edu.tr>
2025-03-04 16:26:10 +0300

    NumberToWords

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,12 +7,13 @@ public class Main {
     public static boolean isPalindrome(int number) {
         int originalNumber=Math.abs(number);
         int reverseNumber=0;
+        int temp = originalNumber;
 
 
-        while (originalNumber>0){
-            int lastDigit=number %10;
+        while (temp>0){
+            int lastDigit=temp %10;
             reverseNumber=reverseNumber * 10+lastDigit;
-            originalNumber/=10;
+            temp/=10;
         }
         return originalNumber==reverseNumber;
     }
@@ -30,4 +31,61 @@ public static boolean isPerfectNumber(int number){
         return sum==number;
 }
 
+public static String numberToWords(int number){
+        if (number<0){
+            return "Invalid Value";
+        }
+        if (number==0){
+            return "Sıfır";
+        }
+
+    String word = "";
+    String result = "";
+    int num = number;
+
+    while (num>0){
+        int digit =num % 10;
+
+        switch (digit) {
+            case 0:
+                word = "Zero";
+                break;
+            case 1:
+                word = "One";
+                break;
+            case 2:
+                word = "Two";
+                break;
+            case 3:
+                word = "Three";
+                break;
+            case 4:
+                word = "Four";
+                break;
+            case 5:
+                word = "Five";
+                break;
+            case 6:
+                word = "Six";
+                break;
+            case 7:
+                word = "Seven";
+                break;
+            case 8:
+                word = "Eight";
+                break;
+            case 9:
+                word = "Nine";
+                break;
+            default:
+                word = ""; // Should not happen, but for safety
+                break;
+        }
+
+        result = word + " " + result;
+        num/=10;
+    }
+return result.trim();
+
+}
 }
